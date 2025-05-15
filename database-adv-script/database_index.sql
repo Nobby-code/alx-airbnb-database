1b81bfb98792df47eb3a8386ce362f0380813cdc
Norbert Guda <102024069+Nobby-code@users.noreply.github.com>
2025-05-15 23:33:09 +0300

    Update database_index.sql

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,1 +1,11 @@
+-- Indexes for users table 
+CREATE INDEX idx_users_id ON users(id); 
+CREATE INDEX idx_users_email ON users(email);
 
+-- Indexes for booking table 
+CREATE INDEX idx_booking_user_id ON booking(user_id); 
+CREATE INDEX idx_booking_property_id ON booking(property_id);
+
+-- Indexes for properties table 
+CREATE INDEX idx_property_id ON property(id); 
+CREATE INDEX idx_property_price ON property(price);
